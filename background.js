f472ed036878e5a46b4528763bbca351d31a0113
Romaanj <88548897+Romaanj@users.noreply.github.com>
2021-08-26 03:40:01 +0900

    404 ERORROR....2nd...

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ const background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 
 const chosenImg = document.createElement("img");
 
-chosenImg.src=`img/${background}`;
+chosenImg.src=background;
 
 document.body.style.backgroundImage="url("+chosenImg.src+")";
 
